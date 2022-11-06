02953a17acef51c276a38607c5a2bfe5b5a082ab
Suraj Misal <surajmisal18@gmail.com>
2022-11-06 17:10:51 +0530

    setup the view engine

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const homecontroller = require('../controllers/home_controller');
 
 router.get('/', homecontroller.home);
+router.use('/users', require('./users'));
+
+// For any other routes, access from here
+// router.use('/routerName', require('./routerFile'));
+
 
 module.exports = router;
