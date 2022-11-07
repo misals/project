d22d8b71d9cc5d81b518308d5d3683fcbb0f9c18
Suraj Misal <surajmisal18@gmail.com>
2022-11-07 12:16:48 +0530

    setup static files access for layout and pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,7 @@
-module.exports.profile = function(req, res) {
-    return res.end('<h1>User profile</h1>');
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
 
 // module.exports.post = function(req, res) {
