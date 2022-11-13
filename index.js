99f4e1db49aca3ad54a24bab6cb9c8fd3a0facb9
Suraj Misal <surajmisal18@gmail.com>
2022-11-13 16:08:22 +0530

    Creating avatar/ Uploading profile picture

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+// make the upload path available to browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 
