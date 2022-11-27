1870604a576a0883411dd91d2c8b278fd005c791
Suraj Misal <surajmisal18@gmail.com>
2022-11-27 14:31:35 +0530

    changed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -26,7 +26,7 @@ module.exports.home = async function(req, res){
     
         let users = await User.find({});
 
-        return res.render('home.ejs', {
+        return res.render('home', {
             title: "Codeial | Home",
             posts:  posts,
             all_users: users
